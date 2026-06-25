fcd66d48811cb2c15d1cb44db72992a7a7ea5592
Deepthi45805 <24311a05kh@cse.sreenidhi.edu.in>
2026-06-25 22:44:47 +0530

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,51 @@
+const API = "http://localhost:5000/tasks";
+
+async function loadTasks() {
+    const res = await fetch(API);
+    const tasks = await res.json();
+
+    const list = document.getElementById("taskList");
+    list.innerHTML = "";
+
+    tasks.forEach(task => {
+        const li = document.createElement("li");
+
+        li.innerHTML = `
+            ${task.title}
+            <button onclick="deleteTask('${task._id}')">
+                Delete
+            </button>
+        `;
+
+        list.appendChild(li);
+    });
+}
+
+async function addTask() {
+    const input = document.getElementById("taskInput");
+
+    if (!input.value) return;
+
+    await fetch(API, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            title: input.value
+        })
+    });
+
+    input.value = "";
+    loadTasks();
+}
+
+async function deleteTask(id) {
+    await fetch(`${API}/${id}`, {
+        method: "DELETE"
+    });
+
+    loadTasks();
+}
+
+loadTasks();
